98e77fceaa66ba26943ec2f8922f75e9383c774b
normanromero06 <norman.romero13607@est.uca.edu.ni>
2023-11-09 19:03:18 -0600

    Me falto subir el proyecto xd

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(Myapp());
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Calculator(),
+   );
+
+  }
+}
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(title: Text('Calculator'),backgroundColor: Colors.black,),
+      body: Padding(padding: EdgeInsets.symmetric(horizontal: 5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          //Display de la calculadora
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(padding: EdgeInsets.all(10.0),
+                child: Text('0',
+                textAlign: TextAlign.left,
+                style: TextStyle(color: Colors.white,
+                fontSize: 100
+                ),                
+                ),
+                ),
+              ],
+                ),
+            )
+        ]
+          ),
+        
+      ),
+    );
+  }
+}
+      
+      
+      
+ 
